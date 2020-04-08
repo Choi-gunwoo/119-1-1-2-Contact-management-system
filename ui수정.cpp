edbec6ac2f3947e9efeb92c2b24a62317d92b461
Choi-gunwoo <63238655+Choi-gunwoo@users.noreply.github.com>
2020-04-08 23:37:43 +0900

    Add files via upload

---
+++ ui수정.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+
+ { 
+       printf("\t**** Welcome to Contact Management ****\n\n");
+	   printf("\t\t\tMAIN MENU\n");
+	   printf("\t\t========================\n");
+	   printf("\t\t[1] Add a new Contact\n");
+	   printf("\t\t[2] List All Contacts\n");
+	   printf("\t\t[3] Search fot contact\n");
+       printf("\t\t[4] Edit a Contact\n");
+       printf("\t\t[5] Delete a Contact\n");
+       printf("\t\t[0] Exit\n");
+       printf("\t\t========================\n");
+       printf("\t\tEnter the choice:");
+       
+       }
